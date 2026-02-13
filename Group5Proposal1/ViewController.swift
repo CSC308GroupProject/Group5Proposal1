98a4ca163566bdc68ce109faed8cce3d99ec557c
Brayden Peeples <braydenpeeples@gmail.com>
2026-02-12 19:26:30 -0500

    Added remaining parts of GUI, including icon, launch screen, and home page design.

--- Group5Proposal1/ViewController.swift
+++ Group5Proposal1/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        button1.layer.cornerRadius = 10
+        button2.layer.cornerRadius = 10
+        button3.layer.cornerRadius = 10
+        
     }
 
 
